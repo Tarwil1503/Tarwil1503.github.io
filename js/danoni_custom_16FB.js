0399b17856bebe95acd1d156a49f2bda80640a6e
Tarwil <93879106+Tarwil1503@users.noreply.github.com>
2022-08-03 01:27:28 +0900

    Add files via upload

---
+++ js/danoni_custom_16FB.js
@@ -0,0 +1,19 @@
+﻿'use strict';
+
+function customTitleInit() {
+
+	
+	g_keycons.colorGroupNum = 1;
+	const keyPtn = ["5_0", "5_1", "5_2"];
+	const viewGroup = _type => {
+		if (g_headerObj[`${_type}Use`]) {
+			keyPtn.forEach (keyCtrlPtn => {
+				if (g_keyObj[`${_type}${keyCtrlPtn}_${g_keycons.colorGroupNum}`] !== undefined) {
+					g_keyObj[`${_type}${keyCtrlPtn}`] =  g_keyObj[`${_type}${keyCtrlPtn}_${g_keycons[`${_type}GroupNum`]}`].concat();
+				}
+			});
+		}
+	}
+	viewGroup("color");
+
+}
